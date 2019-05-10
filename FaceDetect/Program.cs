c889763b690e6b3185364b56229dbc5c86b0a5d8
Hardeep Singh <hsingh@imax.com>
2019-05-10 16:39:10 -0400

    Add project files.

---
+++ FaceDetect/Program.cs
@@ -0,0 +1,129 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+
+using SixLabors.Primitives;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace FaceDetect
+{
+    class Program
+    {
+        private static string msg = "Please provide the API key as the first command-line parameter, followed by the file name of the image to be submitted for processing";
+
+        static void Main(string[] args)
+        {
+            // commandline argumentsw
+
+            var apikey = !string.IsNullOrWhiteSpace(args[0]) ? args[0] : throw new ArgumentException(msg, args[0]);
+            var filename = File.Exists(args[1]) ? args[1] : throw new FileNotFoundException(msg, args[1]);
+
+            //http request
+
+            var region = "westcentralus";
+
+            var target = new Uri($"https://{region}.api.cognitive.microsoft.com/face/v1.0/detect/?subscription-key={apikey}");
+            var httpPost = CreateHttpRequest(target, "POST", "application/octet-stream");
+
+            // load image
+
+            using (var fs = File.OpenRead(filename))
+            {
+                fs.CopyTo(httpPost.GetRequestStream());
+            }
+
+            // submit image to http endpoint 
+
+            string data = GetResponse(httpPost);
+
+            //inspect json
+
+          var rectangles =  GetRectangles(data);
+
+
+            // draw rectangles on the image (copy image)
+
+            var img = Image.Load(filename);
+            var count = 0;
+            foreach (var rectangle in GetRectangles(data))
+            {
+                img.Mutate(a => a.DrawPolygon(Rgba32.HotPink, 20, rectangle));
+                count++;
+            }
+            Console.WriteLine($"Number of faces detected: {count}");
+            var outputfilename = $"{Environment.CurrentDirectory}\\{Path.GetFileNameWithoutExtension(filename)}-2{Path.GetExtension(filename)}";
+            SaveImage(img, outputfilename);
+
+            OpenWithDefaultApp(outputfilename);
+        }
+
+        private static void OpenWithDefaultApp(string filename)
+        {
+            var si = new ProcessStartInfo()
+            {
+                FileName = "explorer.exe",
+                Arguments = filename,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            Process.Start(si);
+        }
+
+        private static void SaveImage(Image<Rgba32> img, string outputfilename)
+        {
+            using (var fs = File.Create(outputfilename))
+            {
+                img.SaveAsJpeg(fs);
+
+            }
+        }
+
+        private static IEnumerable<PointF[]> GetRectangles(string data)
+        {
+            var faces = JArray.Parse(data);
+            foreach (var face in faces)
+            {
+               // var id = (string)faces["faceid"];
+
+                var top = (int)face["faceRectangle"]["top"];
+                var left = (int)face["faceRectangle"]["left"];
+                var width = (int)face["faceRectangle"]["width"];
+                var height = (int)face["faceRectangle"]["height"];
+
+                var rectangle = new PointF[]
+                {
+                    new PointF(left,top),
+                    new PointF(left+width,top),
+                    new PointF(left + width,top + height),
+                    new PointF(left,top + height),
+
+
+                };
+
+                yield return rectangle;
+
+            }
+        }
+
+        private static string GetResponse(HttpWebRequest httpPost)
+        {
+            using (var response = httpPost.GetResponse())
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        private static HttpWebRequest CreateHttpRequest(Uri target, string method, string contentType)
+        {
+            var request = WebRequest.CreateHttp(target);
+            request.Method = method;
+            request.ContentType = contentType;
+            return request;
+        }
+    }
+}
---
+++ Practice_Basic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] ary = new int[3] { 1, 2, 3 };
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
